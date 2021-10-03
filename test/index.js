9b990ef87c60e6b4a0c45e27c5ef3af21ebcee1e
Andrew Kirwin <andrew.kirwin@zestia.com>
2021-10-03 20:02:50 +0100

    start migrating tests to qunit

--- test/index.js
+++ test/index.js
@@ -5,13 +5,44 @@ const {
 } = window;
 
 module('getPosition', function (hooks) {
+  let element;
+  let container;
+
   hooks.beforeEach(function () {
-    // todo
+    element = document.getElementById('test-element');
+    container = document.getElementById('test-container');
   });
 
-  test('css animations', function (assert) {
+  test('possible positions', function (assert) {
     assert.expect(1);
 
-    assert.true(true);
+    assert.equal(getPosition(element, container, 3, 3), 'top left');
+  });
+});
+
+module('getCoords', function (hooks) {
+  let element;
+  let reference;
+
+  hooks.beforeEach(function () {
+    element = document.getElementById('test-element');
+    reference = document.getElementById('test-reference');
+  });
+
+  test('possible positions', function (assert) {
+    assert.expect(12);
+
+    assert.deepEqual(getCoords('top left', element, reference), [0, 150]);
+    assert.deepEqual(getCoords('top center', element, reference), [50, 150]);
+    assert.deepEqual(getCoords('top right', element, reference), [100, 150]);
+    assert.deepEqual(getCoords('right top', element, reference), [200, 200]);
+    assert.deepEqual(getCoords('right middle', element, reference), [200, 225]);
+    assert.deepEqual(getCoords('right bottom', element, reference), [200, 250]);
+    assert.deepEqual(getCoords('bottom left', element, reference), [0, 300]);
+    assert.deepEqual(getCoords('bottom center', element, reference), [50, 300]);
+    assert.deepEqual(getCoords('bottom right', element, reference), [100, 300]);
+    assert.deepEqual(getCoords('left top', element, reference), [-100, 200]);
+    assert.deepEqual(getCoords('left middle', element, reference), [-100, 225]);
+    assert.deepEqual(getCoords('left bottom', element, reference), [-100, 250]);
   });
 });
