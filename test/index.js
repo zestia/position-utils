a38e94f8df00f2ac1481cccab13fd8b28e097d1e
Andrew Kirwin <andrew.kirwin@zestia.com>
2021-10-03 20:58:03 +0100

    add tests

--- test/index.js
+++ test/index.js
@@ -16,43 +16,69 @@ module('getPosition', function (hooks) {
   test('possible positions', function (assert) {
     assert.expect(1);
 
-    assert.equal(getPosition(element, container, 3, 3), 'top left');
+    // assert.equal(getPosition(element, container, 3, 3), 'top left');
   });
 });
 
 module('getCoords', function (hooks) {
   let element;
   let reference;
+  let container;
 
   hooks.beforeEach(function () {
     element = document.getElementById('test-element');
     reference = document.getElementById('test-reference');
+    container = document.getElementById('test-container');
+  });
+
+  hooks.afterEach(function () {
+    reference.style = '';
+    container.style = '';
+    container.scrollTop = 0;
   });
 
   test('possible positions', function (assert) {
     assert.expect(12);
 
-    assert.deepEqual(getCoords('top left', element, reference), [0, 150]);
-    assert.deepEqual(getCoords('top center', element, reference), [50, 150]);
-    assert.deepEqual(getCoords('top right', element, reference), [100, 150]);
-    assert.deepEqual(getCoords('right top', element, reference), [200, 200]);
-    assert.deepEqual(getCoords('right middle', element, reference), [200, 225]);
-    assert.deepEqual(getCoords('right bottom', element, reference), [200, 250]);
-    assert.deepEqual(getCoords('bottom left', element, reference), [0, 300]);
-    assert.deepEqual(getCoords('bottom center', element, reference), [50, 300]);
-    assert.deepEqual(getCoords('bottom right', element, reference), [100, 300]);
-    assert.deepEqual(getCoords('left top', element, reference), [-100, 200]);
-    assert.deepEqual(getCoords('left middle', element, reference), [-100, 225]);
-    assert.deepEqual(getCoords('left bottom', element, reference), [-100, 250]);
+    assert.deepEqual(getCoords('top left', element, reference), [0, -50]);
+    assert.deepEqual(getCoords('top center', element, reference), [50, -50]);
+    assert.deepEqual(getCoords('top right', element, reference), [100, -50]);
+    assert.deepEqual(getCoords('right top', element, reference), [200, 0]);
+    assert.deepEqual(getCoords('right middle', element, reference), [200, 25]);
+    assert.deepEqual(getCoords('right bottom', element, reference), [200, 50]);
+    assert.deepEqual(getCoords('bottom left', element, reference), [0, 100]);
+    assert.deepEqual(getCoords('bottom center', element, reference), [50, 100]);
+    assert.deepEqual(getCoords('bottom right', element, reference), [100, 100]);
+    assert.deepEqual(getCoords('left top', element, reference), [-100, 0]);
+    assert.deepEqual(getCoords('left middle', element, reference), [-100, 25]);
+    assert.deepEqual(getCoords('left bottom', element, reference), [-100, 50]);
   });
 
   test('offset parent', function (assert) {
     assert.expect(1);
 
     reference.style.position = 'relative';
-    reference.style.top = '5px';
-    reference.style.left = '10px';
+    reference.style.left = '1px';
+    reference.style.top = '2px';
 
-    assert.deepEqual(getCoords('bottom right', element, reference), [110, 305]);
+    assert.deepEqual(getCoords('bottom right', element, reference), [101, 102]);
+  });
+
+  test('hidden parent', function (assert) {
+    assert.expect(1);
+
+    container.style.display = 'none';
+
+    assert.deepEqual(getCoords('bottom right', element, reference), [0, 0]);
+  });
+
+  test('scrolled parent', function (assert) {
+    assert.expect(1);
+
+    container.style.overflow = 'scroll';
+    reference.style.marginTop = '400px';
+    container.scrollTop = 101;
+
+    assert.deepEqual(getCoords('bottom right', element, reference), [100, 399]);
   });
 });
